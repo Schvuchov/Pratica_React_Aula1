9f13e008e1d863dcb23f7ec72cef403b50e450b4
Barbara Schvuchov <schvuchov.k@gmail.com>
2023-11-21 14:05:21 -0300

    componentes

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,20 @@
+import "./styles.css"
+
+
+export function Menu(){
+   return(
+       <div id="links">
+           <a target="_black" href="#">
+               Destinos
+           </a>
+
+           <a target="_black" href="#">
+               Ofertas
+           </a>
+
+           <a href="mailto:brunobandeirafernandes@gmail.com">
+               Contato
+           </a>
+       </div>
+   )
+}
